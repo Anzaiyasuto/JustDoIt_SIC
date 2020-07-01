a02f0fe47201ff89b5cd14e2f3eb49336e25a21a
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2020-07-01 17:51:34 +0900

    入力、変更画面のシークバー作成ver2 データベースにタスク完了フラグを追加しました。

--- app/src/main/java/com/example/myscheduler/Schedule.kt
+++ app/src/main/java/com/example/myscheduler/Schedule.kt
@@ -10,7 +10,8 @@ import java.util.*
 open class Schedule : RealmObject() {
     @PrimaryKey var id: Long = 0 //タスクID
     var date: Date = Date() //時間(yyyymmdd)
-    var progress_data: Int = 0
+    var progress_data: Int = 0 //進捗度
     var title: String = ""  //タスク名
     var detail: String = "" //タスク詳細
+    var completeFlag: Int = 0
 }
