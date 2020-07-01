15858e8f7de6d14816734ac35d4c36125926e31a
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2020-07-02 01:03:24 +0900

    入力、変更画面のシークバー作成=完成しました

--- app/src/main/java/com/example/myscheduler/ScheduleEditActivity.kt
+++ app/src/main/java/com/example/myscheduler/ScheduleEditActivity.kt
@@ -22,23 +22,54 @@ class ScheduleEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule_edit)
+        //データベース用のインスタンスを用意
         realm = Realm.getDefaultInstance()
 
-        val scheduleId = intent?.getLongExtra("schedule_id", -1L)
-
         // seekbar 初期値
-        progress_seekbar.progress = 0
+        //progress_seekbar.progress = 0
         // seekbar 最大値
         progress_seekbar.max = 100
+        //シークバーを触った時の処理系
+        progress_seekbar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                //ツマミがドラッグされると呼ばれる
+                override fun onProgressChanged(
+                    seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    // 68 % のようにフォーマト、
+                    // この場合、Locale.USが汎用的に推奨される
+                    val str = String.format(Locale.US, "%d %%", progress)
+                    progress_text.text = str
+                }
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+                    // ツマミがタッチされた時に呼ばれる
+                }
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                    // ツマミがリリースされた時に呼ばれる
+                }
+            })
+        //scheduleId=nullの時-1Lを代入->新規作成の時に実行される。
+        val scheduleId = intent?.getLongExtra("schedule_id", -1L)
 
 
         if (scheduleId != -1L) {
+            //既存タスクの時
             val schedule = realm.where<Schedule>()
                 .equalTo("id", scheduleId).findFirst()
             dateEdit.setText(DateFormat.format("yyyy/MM/dd", schedule?.date))
             titleEdit.setText(schedule?.title)
+            if (schedule != null) {
+                progress_seekbar.progress = schedule.progress_data
+                val str = String.format(Locale.US, "%d %%", schedule.progress_data)
+                progress_text.text = str
             delete.visibility = View.VISIBLE
         } else {
+            //新規タスクを見るとき
+            realm.executeTransaction{ db: Realm ->
+                val schedule = db.where<Schedule>()
+                    .equalTo("id", scheduleId).findFirst()
+
+                }
+            }
             delete.visibility = View.INVISIBLE
         }
 
@@ -50,11 +81,11 @@ class ScheduleEditActivity : AppCompatActivity() {
                         val nextId = (maxId?.toLong() ?: 0L) + 1
 
                         val schedule = db.createObject<Schedule>(nextId)
-                        schedule.progress_data = 0
                         val date = dateEdit.text.toString().toDate("yyyy/MM/dd")
                         if (date != null) schedule.date = date
                         schedule.title = titleEdit.text.toString()
-
+                        schedule.progress_data = progress_seekbar.progress
+                        schedule.completeFlag = 0
                     }
                     Snackbar.make(view, "追加しました", Snackbar.LENGTH_SHORT)
                         .setAction("戻る") { finish() }
@@ -62,8 +93,6 @@ class ScheduleEditActivity : AppCompatActivity() {
                         .show()
                 }
                 else -> {
-
-
                     realm.executeTransaction { db: Realm ->
 
                         val schedule = db.where<Schedule>()
@@ -72,31 +101,10 @@ class ScheduleEditActivity : AppCompatActivity() {
                             .toDate("yyyy/MM/dd")
                         if (date != null) schedule?.date = date
                         schedule?.title = titleEdit.text.toString()
-                        progress_seekbar.progress = 0
+                        // seekbar 初期値
                         if (schedule != null) {
-                            progress_seekbar.progress = schedule.progress_data
+                            schedule.progress_data = progress_seekbar.progress
                         }
-
-                        progress_seekbar.setOnSeekBarChangeListener(
-                            object : SeekBar.OnSeekBarChangeListener {
-                                //ツマミがドラッグされると呼ばれる
-                                override fun onProgressChanged(
-                                    seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                                    // 68 % のようにフォーマト、
-                                    // この場合、Locale.USが汎用的に推奨される
-                                    val str = String.format(Locale.US, "%d %%", progress)
-                                    progress_text.text = str
-                                }
-                                override fun onStartTrackingTouch(seekBar: SeekBar) {
-                                    // ツマミがタッチされた時に呼ばれる
-
-                                }
-                                override fun onStopTrackingTouch(seekBar: SeekBar) {
-                                    // ツマミがリリースされた時に呼ばれる
-
-                                }
-                            })
-
                     }
                     Snackbar.make(view, "修正しました", Snackbar.LENGTH_SHORT)
                         .setAction("戻る") { finish() }
